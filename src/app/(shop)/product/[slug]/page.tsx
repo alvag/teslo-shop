bd81ae1d304524171f1ea1bf941dd3c93c7ac953
Max Alva <alva85@gmail.com>
2024-01-09 08:40:07 -0500

    Product page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,11 +1,65 @@
 import { FC } from 'react';
+import { initialData } from '@/seed/seed';
+import { notFound } from 'next/navigation';
+import { titleFont } from '@/config/fonts';
+import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from '@/components';
 
 interface ProductProps {
+    params: {
+        slug: string;
+    };
 }
 
-const ProductPage: FC<ProductProps> = () => {
+const ProductPage: FC<ProductProps> = ( { params } ) => {
+    const { slug } = params;
+    const product = initialData.products.find( ( product ) => product.slug === slug );
+
+    if ( !product ) {
+        notFound();
+    }
+
     return (
-        <div>Product Page</div>
+        <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+            <div className="col-span-1 md:col-span-2">
+
+                <ProductMobileSlideshow
+                    title={ product.title }
+                    images={ product.images }
+                    className="block md:hidden"
+                />
+
+                <ProductSlideshow
+                    title={ product.title }
+                    images={ product.images }
+                    className="hidden md:block"
+                />
+
+            </div>
+
+            <div className="col-span-1 px-5">
+                <h1 className={ `${ titleFont.className } antialiased font-bold text-xl` }>
+                    { product.title }
+                </h1>
+
+                <p className="text-lg mb-5">
+                    { product.price }
+                </p>
+
+                <SizeSelector selectedSize={ product.sizes[ 0 ] } availableSizes={ product.sizes }/>
+
+                <QuantitySelector quantity={ 2 }/>
+
+                <button className="btn-primary my-5">
+                    Agregar al carrito
+                </button>
+
+                <h3 className="font-bold text-sm">Descripción</h3>
+
+                <p className="font-light">
+                    { product.description }
+                </p>
+            </div>
+        </div>
     );
 };
 
---
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { CSSProperties, useState } from 'react';
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper as SwiperObject } from 'swiper';
+import { Autoplay, FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import './slideshow.css';
+
+interface ProductSlideshowProps {
+    images: string[];
+    title: string;
+    className?: string;
+}
+
+export const ProductSlideshow = ( { images, title, className }: ProductSlideshowProps ) => {
+
+    const [ thumbsSwiper, setThumbsSwiper ] = useState<SwiperObject>();
+
+    return (
+        <div className={ className }>
+            <Swiper
+                style={ { '--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff' } as CSSProperties }
+                spaceBetween={ 10 }
+                navigation={ true }
+                autoplay={ { delay: 2500 } }
+                thumbs={ { swiper: thumbsSwiper } }
+                modules={ [ FreeMode, Navigation, Thumbs, Autoplay ] }
+                className="mySwiper2"
+            >
+                {
+                    images.map( ( image ) => (
+                        <SwiperSlide key={ image }>
+                            <Image src={ `/products/${ image }` }
+                                   width={ 1024 }
+                                   height={ 800 }
+                                   className="rounded-lg object-fill"
+                                   alt={ title }/>
+                        </SwiperSlide>
+                    ) )
+                }
+            </Swiper>
+
+            <Swiper
+                onSwiper={ setThumbsSwiper }
+                spaceBetween={ 10 }
+                slidesPerView={ 4 }
+                freeMode={ true }
+                watchSlidesProgress={ true }
+                modules={ [ FreeMode, Navigation, Thumbs ] }
+                className="mySwiper"
+            >
+                {
+                    images.map( ( image ) => (
+                        <SwiperSlide key={ image }>
+                            <Image src={ `/products/${ image }` }
+                                   width={ 300 }
+                                   height={ 300 }
+                                   className="rounded-lg object-fill"
+                                   alt={ title }/>
+                        </SwiperSlide>
+                    ) )
+                }
+            </Swiper>
+        </div>
+    );
+};
